adae511777f842db4d5afaa122b97209b4ded808
eTrinityE <64984047+eTrinityE@users.noreply.github.com>
2023-06-06 21:34:49 -0300

    Update ImageService.java

--- src/main/java/com/iftm/ecommerce/services/ImageService.java
+++ src/main/java/com/iftm/ecommerce/services/ImageService.java
@@ -1,9 +1,15 @@
 package com.iftm.ecommerce.services;
 
+import com.iftm.ecommerce.data.vo.ImageVO;
+import com.iftm.ecommerce.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ImageService {
-
+    @Autowired
+    private ImageRepository imageRepository;
+    
 }
