2d3670cc099dd30fc81c83d1f8f498be2a7ea6db
Samuell0003 <2003samuellucas@gmail.com>
2023-06-17 01:40:41 -0300

    controller, service and database configuration

--- src/main/java/com/iftm/ecommerce/controllers/ProductController.java
+++ src/main/java/com/iftm/ecommerce/controllers/ProductController.java
@@ -1,12 +1,26 @@
 package com.iftm.ecommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.iftm.ecommerce.data.vo.ProductVO;
 import com.iftm.ecommerce.services.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("api/v1/product")
@@ -15,5 +29,41 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @PostMapping
+    @Operation(
+        summary = "Create a product.", description = "Create a product.", tags = {"Product"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                content = @Content(
+                    schema = @Schema(implementation = ProductVO.class)
+                )
+            ),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
+    public ProductVO insert(@RequestBody ProductVO product) {
+        return service.save(product);
+    }
+
+
+
+
+    @GetMapping
+    public List<ProductVO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductVO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        service.deleteById(id);
+    }
     
 }
--- src/main/java/com/iftm/ecommerce/services/ProductService.java
+++ src/main/java/com/iftm/ecommerce/services/ProductService.java
@@ -1,13 +1,39 @@
 package com.iftm.ecommerce.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.iftm.ecommerce.data.vo.ProductVO;
+import com.iftm.ecommerce.mapper.DozerMapper;
+import com.iftm.ecommerce.models.Product;
 import com.iftm.ecommerce.repositories.ProductRepository;
+
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    
+    public ProductVO save(ProductVO productVO) {
+        Product product = DozerMapper.parseObject(productVO, Product.class);
+        var userDb = repository.save(product);
+        productVO = DozerMapper.parseObject(userDb, ProductVO.class);
+
+        return productVO;
+    }
+
+
+    public List<ProductVO> findAll() {
+        return DozerMapper.parseListObject(repository.findAll(), ProductVO.class);
+    }
+
+    public ProductVO findById(Long id) {
+        return DozerMapper.parseObject(repository.findById(id).get(),  ProductVO.class);
+
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
