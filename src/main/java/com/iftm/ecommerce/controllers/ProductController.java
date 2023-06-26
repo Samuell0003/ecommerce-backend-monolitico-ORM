74fed8bdb5367a34ceaa8bbd1958346e59443967
Samuell0003 <2003samuellucas@gmail.com>
2023-06-25 23:14:49 -0300

    swagger configuration

--- src/main/java/com/iftm/ecommerce/controllers/ProductController.java
+++ src/main/java/com/iftm/ecommerce/controllers/ProductController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.iftm.ecommerce.data.vo.ProductVO;
 import com.iftm.ecommerce.services.ProductService;
+import com.iftm.ecommerce.utils.MediaType;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -29,7 +31,8 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @PostMapping
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+                 produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     @Operation(
         summary = "Create a product.", description = "Create a product.", tags = {"Product"},
         responses = {
@@ -44,25 +47,62 @@ public class ProductController {
             @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
         }
     )
-    public ProductVO insert(@RequestBody ProductVO product) {
+    public ProductVO insert(@RequestBody ProductVO product) throws Exception {
         return service.save(product);
     }
 
 
 
 
-    @GetMapping
+    @GetMapping(produces =  { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(
+        summary = "Find all.", description = "Find all products.", tags = {"Product"},
+        responses = {
+            @ApiResponse(description = "Sucess.", responseCode = "200",
+            content = {
+                @Content(mediaType = MediaType.APPLICATION_JSON,
+                    array = @ArraySchema(schema = @Schema(implementation = ProductVO.class)))
+                }
+            ),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public List<ProductVO> findAll() {
         return service.findAll();
     }
-
-    @GetMapping("/{id}")
-    public ProductVO findById(@PathVariable("id") Long id) {
+    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(
+        summary = "Find by Id", description = "Product find by ID", tags = {"Product"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                content = @Content( mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = ProductVO.class)
+                )
+            ),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
+    public ProductVO findById(@PathVariable("id") Long id) throws Exception {
         return service.findById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable("id") Long id) {
+    @Operation(
+        summary = "delete by Id", description = "Product delete by ID", tags = {"Product"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200"),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
+    public void deleteById(@PathVariable("id") Long id) throws Exception {
         service.deleteById(id);
     }
     
---
+++ src/main/java/com/iftm/ecommerce/utils/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,14 @@
+package com.iftm.ecommerce.utils.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+
+public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+    public YamlJackson2HttpMessageConverter() {
+        super(new YAMLMapper().setSerializationInclusion(
+                        JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/x-yaml"));
+    }
+}
